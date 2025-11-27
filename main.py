6f028fc894b8c80658bd728fe9a9b893411443a8
Jordan <trainee.jordan.ogunseye@sigmalabs.co.uk>
2025-11-27 10:28:18 +0000

    Inital commit

---
+++ main.py
@@ -0,0 +1,9 @@
+"""Who knows"""
+
+
+def add(x: int, y: int) -> int:
+    pass
+
+
+if __name__ == "__main__":
+    pass
---
+++ test_main.py
@@ -0,0 +1,5 @@
+from main import add
+
+
+def test_addition_valid():
+    assert add(3, 4) == 7
